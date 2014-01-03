f5f1eb3666c4a81a25cebdef7103a36c7a9acf96
Rob Smit <rob.smit@ag5.nl>
2014-01-03 11:24:40 +0100

    double extenions

--- src/main/java/st/gravel/support/jvm/DoubleExtensions.java
+++ src/main/java/st/gravel/support/jvm/DoubleExtensions.java
@@ -1,12 +1,39 @@
 package st.gravel.support.jvm;
 
 public class DoubleExtensions {
-	public static double sqrt(double receiver) {
-		return Math.sqrt(receiver);
-	}
-
 	public static float asFloat(double receiver) {
 		return (float) receiver;
 	}
 
+	public static double ln(double receiver) {
+		return Math.log(receiver);
+	}	
+	
+	public static double sqrt(double receiver) {
+		return Math.sqrt(receiver);
+	}
+	
+	public static double sin(double receiver) {
+		return Math.sin(receiver);
+	}
+	
+	public static double cos(double receiver) {
+		return Math.cos(receiver);
+	}
+	
+	public static double tan(double receiver) {
+		return Math.tan(receiver);
+	}
+	
+	public static double arcSin(double receiver) {
+		return Math.asin(receiver);
+	}
+	
+	public static double arcCos(double receiver) {
+		return Math.acos(receiver);
+	}
+	
+	public static double arcTan(double receiver) {
+		return Math.atan(receiver);
+	}
 }
